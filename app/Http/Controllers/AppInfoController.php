3e12d9568bf8cf2dcb2618faccd140a3e95f225a
Christofer Bodin <christofer@christoferbodin.se>
2019-01-04 02:17:33 +0100

    Add download url of latest android version to app info route.

--- app/Http/Controllers/AppInfoController.php
+++ app/Http/Controllers/AppInfoController.php
@@ -40,6 +40,7 @@ class AppInfoController extends Controller
         return [
             'latest_version' => $latestVersion,
             'is_latest' => $latestVersion === $version,
+            'download_url' => url("releases/veganresan-$latestVersion.apk"),
             'news' => $news,
         ];
     }
