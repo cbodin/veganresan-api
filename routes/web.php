e2e6b46fa6523644fc678b3a480a868e08684edd
Christofer Bodin <christofer@christoferbodin.se>
2019-01-04 01:05:05 +0100

    Group meal and link create routes in auth middleware.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
-/* @var $router \Laravel\Lumen\Routing\Router */
 
+use Laravel\Lumen\Routing\Router;
+
+/* @var $router Router */
 $router->get('/mobile-version/android', 'MobileAppVersionController@android');
-
-$router->post('/meal/create', 'MealAdminController@create');
-$router->post('/link/create', 'MealAdminController@createLink');
-
 $router->get('/meal/list', 'MealController@list');
+
+$router->group(['middleware' => 'auth'], function (Router $router) {
+    $router->post('/meal/create', 'MealAdminController@create');
+    $router->post('/link/create', 'MealAdminController@createLink');
+});
